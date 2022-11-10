1748040a2bb1da9476d1a32a416dfa75f0359c50
Fansy <418536538@qq.com>
2022-11-11 00:06:41 +0800

    feat: add version command

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,10 +1,11 @@
 import { Command } from 'commander';
-import { name } from '../package.json';
+import { name, version } from '../package.json';
 import { lint } from '../lib';
 
 const program = new Command();
 
 program.name(name).usage('[command] [options]');
+program.version(version, '-v, --version');
 
 program
     .command('lint <message>')
